a47ace05d55ab209269518c230d9ab7742b8d808
Lalli-Leib <lauri.t.ahonen@gmail.com>
2025-08-04 16:19:04 +0300

    1.11
    Unicafe loppuun, onnistu jo vähän paremmin

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,19 +8,32 @@ const Header2 = ({ otsikko2 }) => {
   return <h2>{otsikko2}</h2>
 }
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, teksti }) => {
+  return <button onClick={handleClick}>{teksti}</button>
+}
+
+const StatisticLine = ({ teksti, arvo }) => {
+  return (
+    <tr>
+      <td>{teksti}</td>
+      <td>{arvo}</td>
+    </tr>
+  )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return <p>No feedback given</p>
+  }
+
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) }</p>
-      <p>positive {(good/(neutral + bad + good)*100).toFixed(1)}%</p>
+      <StatisticLine teksti="good" arvo={good} />
+      <StatisticLine teksti="neutral" arvo={neutral} />
+      <StatisticLine teksti="bad" arvo={bad} />
+      <StatisticLine teksti="all" arvo={good + neutral + bad} />
+      <StatisticLine teksti="average" arvo={(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)} />
+      <StatisticLine teksti="positive" arvo={(good / (neutral + bad + good) * 100).toFixed(1) + '%'} />
     </div>
   )
 }
@@ -28,7 +41,6 @@ const Statistics = ({ good, neutral, bad }) => {
 const App = () => {
   const otsikko = 'give feedback'
   const otsikko2 = 'statistics'
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -36,11 +48,12 @@ const App = () => {
   return (
     <div>
       <Header1 otsikko1={otsikko} />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} teksti="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} teksti="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} teksti="bad" />
       <Header2 otsikko2={otsikko2} />
       <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
