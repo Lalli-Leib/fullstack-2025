8340074be680b75b52aa51d96984f8116a998ab6
Lalli-Leib <lauri.t.ahonen@gmail.com>
2025-08-18 11:57:47 +0300

    Lisää tehtäviä

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personsService from './services/persons'
 
-const Filter = ({ filter, handleFilterChange }) => (
+const Filter = ({ filter, handleFilter }) => (
   <div>
-    Filter shown with: <input value={filter} onChange={handleFilterChange} />
+    filter shown with: <input value={filter} onChange={handleFilter} />
   </div>
 )
 
 const PersonForm = ({ addPerson, newName, handleName, newNumber, handleNumber }) => (
   <form onSubmit={addPerson}>
     <div>
-      Name: <input value={newName} onChange={handleName} />
+      name: <input value={newName} onChange={handleName} />
     </div>
     <div>
-      Number: <input value={newNumber} onChange={handleNumber} />
+      number: <input value={newNumber} onChange={handleNumber} />
     </div>
     <div>
       <button type="submit">add</button>
@@ -20,52 +21,53 @@ const PersonForm = ({ addPerson, newName, handleName, newNumber, handleNumber })
   </form>
 )
 
-const Person = ({ person }) => (
-  <li>{person.name} {person.number}</li>
+const Person = ({ person, handleDelete }) => (
+  <li>
+    {person.name} {person.number}
+    <button onClick={() => handleDelete(person.id, person.name)}>delete</button>
+  </li>
 )
 
-const Persons = ({ persons }) => (
+const Persons = ({ persons, handleDelete }) => (
   <ul>
     {persons.map(person => (
-      <Person key={person.name} person={person} />
+      <Person key={person.id} person={person} handleDelete={handleDelete} />
     ))}
   </ul>
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('') 
 
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        console.log('Palvelimelta haettu data:', initialPersons)
+        setPersons(initialPersons)
+      })
+  }, [])
+
   const handleName = (event) => {
-    console.log('Nimikenttään kirjoitettu:', event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumber = (event) => {
-    console.log('Numerokenttään kirjoitettu:', event.target.value)
     setNewNumber(event.target.value)
   }
 
-  const handleFilterChange = (event) => {
-    console.log('Suodatin:', event.target.value)
+  const handleFilter = (event) => {
     setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('Lomakkeen lähetys. Nimi:', newName, 'Numero:', newNumber)
-
     const trimmedName = newName.trim()
+
     if (persons.some(person => person.name === trimmedName)) {
-      console.log(`Henkilö ${trimmedName} on jo olemassa`)
       alert(`${trimmedName} is already added to phonebook`)
       return
     }
@@ -75,10 +77,27 @@ const App = () => {
       number: newNumber.trim()
     }
 
-    console.log('Lisätään henkilö:', nameObject)
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
+    console.log('Lisätään uusi henkilö palvelimelle:', nameObject)
+
+    personsService
+      .create(nameObject)
+      .then(returnedPerson => {
+        console.log('Palautettu lisäyksen jälkeen:', returnedPerson)
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personsService
+        .rmv(id)
+        .then(() => {
+          console.log(`Poistettu ${name} palvelimelta`)
+          setPersons(persons.filter(p => p.id !== id))
+        })
+    }
   }
 
   const personsToShow = persons.filter(person =>
@@ -86,22 +105,23 @@ const App = () => {
   )
 
   return (
-
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <h2>add a new person</h2>
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <h2>add a new</h2>
       <PersonForm 
         addPerson={addPerson}
         newName={newName}
         handleName={handleName}
         newNumber={newNumber}
-        handleNumber={handleNumber}/>
+        handleNumber={handleNumber}
+      />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} handleDelete={deletePerson} />
     </div>
-
   )
 }
 
 export default App ///ÄLÄ POISTA
+
+///ET OO TEHNY 2.15!!!!!
