61da55b40330762ef2041c38a8137d1161f97f8d
Lalli-Leib <lauri.t.ahonen@gmail.com>
2025-08-20 13:38:07 +0300

    poistettu cors ja lisätty proxy

--- part3/puhelinluettelo_be/index.js
+++ part3/puhelinluettelo_be/index.js
@@ -2,12 +2,10 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('cors')
 let persons = require('./data/persons')
 
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/api/persons/:id', (request, response) => {
