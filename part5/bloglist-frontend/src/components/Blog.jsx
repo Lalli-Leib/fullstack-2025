db823d11c6a3d128930ae7496836a28741fb4f6d
Lalli-Leib <lauri.t.ahonen@gmail.com>
2025-09-03 17:30:49 +0300

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, onLike }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogMax = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 10
+  }
+
+  const blogMin = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 10,
+    marginTop: 10
+  }
+
+  return (
+    <div>
+      <div style={blogMin}>
+        {blog.title}
+        <button style={{ marginLeft: '10px' }} onClick={toggleVisibility}> {visible ? 'hide' : 'view'}</button>
+      </div>
+      {visible && (
+        <div style={blogMax}>
+          <div>Url: {blog.url}</div>
+          <div>Likes: {blog.likes} <button onClick={onLike}>like</button> </div>
+          <div>Author: {blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
